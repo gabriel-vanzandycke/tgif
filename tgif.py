0b737484be1db9679d43bcf07cffe11be4397f47
Gabriel Van Zandycke <gabriel.vanzandycke@gmail.com>
2020-05-01 16:12:26 +0200

    corrected replace in file

--- tgif.py
+++ tgif.py
@@ -4,6 +4,7 @@ import datetime
 import schedule
 import time
 import boto3
+import shutil
 from holidays import official_tgif, get_holidays
 
 
@@ -27,24 +28,25 @@ class AWSS3Client(object):
 
 s3_client = AWSS3Client("auie", "gva@DEV")
 
-def replace(src_filename, dst_filename, src_string, dst_string):
+def replace(filename, src_string, dst_string):
     # Read in the file
-    with open(src_filename, 'r') as file :
+    with open(filename, 'r') as file :
         filedata = file.read()
 
     # Replace the target string
     filedata = filedata.replace(src_string, dst_string)
 
     # Write the file out again
-    with open(dst_filename, 'w') as file:
+    with open(filename, 'w') as file:
         file.write(filedata)
 
 
 def job(*_): # argument unused for now
     holidays = get_holidays()
     days, exponent = official_tgif(holidays)
-    replace("tgif.html.template", "tgif.html", "#DAYS#", str(days))
-    replace("tgif.html.template", "tgif.html", "#EXPONENT#", str(exponent))
+    shutil.copyfile("tgif.html.template", "tgif.html")
+    replace("tgif.html", "#DAYS#", str(days))
+    replace("tgif.html", "#EXPONENT#", str(exponent))
     s3_client.upload("tgif.html")
 
 schedule.every().day.at("07:00").do(job, None)
